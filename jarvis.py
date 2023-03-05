4008237b5a3486288f052152580484971acc1a46
Ayush Gupta <gupta07ayush@gmail.com>
2023-03-05 22:06:57 +0530

    voice 1:Zira

--- jarvis.py
+++ jarvis.py
@@ -4,6 +4,10 @@ import pyttsx3
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
+# print(voices[0].id) # two id available 0=David and 1=Zira
+engine.setProperty('voice', voices[1].id)
+
+
 # You can select any voice out of two.
 # voices = engine.getProperty('voices')
 # print(voices)
