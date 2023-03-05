29e5db9410ba1635f322a2946b7000c4284d6867
Ayush Gupta <gupta07ayush@gmail.com>
2023-03-05 22:14:23 +0530

    wishme function

--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 
 # sapi5 is a windows api which provides speech functions
 engine = pyttsx3.init('sapi5')
@@ -11,6 +12,17 @@ engine.setProperty('voice', voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning")
+    elif hour >= 12 and hour < 18:
+        speak("Good AfterNoon")
+    else:
+        speak("Good Evening")
+    speak("I am Jarvis Sir, Please tell me how may I help you?")
 
 
 # print how many voices available.
